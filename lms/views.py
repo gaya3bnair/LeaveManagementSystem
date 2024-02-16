b1d09f9bf2af4dafbd16290fea835e021feab5cf
kirtisikka <kirtisikka972@gmail.com>
2024-02-16 08:38:53 +0530

    updated views

--- lms/views.py
+++ lms/views.py
@@ -13,11 +13,13 @@ from dateutil.relativedelta import relativedelta
 from datetime import datetime
 def landing_page(request):
     return render(request, 'dashboard/landingpage.html')
+
+
 def members_list(request):
     members = Members.objects.all()
     value = False
     val = False
-
+    # print(request.user.id)
 
     for det in members:
         uname = det.username
@@ -28,16 +30,107 @@ def members_list(request):
     return render(request, 'dashboard/members_list.html', {'members': members, "value": value, "val": val})
 
 
-
+# def my_view(request):
+#     user = request.user
+#     context = {'user': user}
+#     return render(request, 'my_template.html', context)
 
 
 def leave_request(request):
+    if request.method == 'POST':
+        start_date = request.POST.get('start-date')
+        end_date = request.POST.get('end-date')
+        reason = request.POST.get('reason')
+        mem = Members.objects.all()
+        for member in mem:
+            username = member.username
+            if username == request.user.username:
+                email = member.mentoremail
+        maillist = email.split(", ")
+        msg = EmailMultiAlternatives(
+            'Leave request', f'Start Date: {start_date}\nEnd Date: {end_date}\nReason: {reason}', EMAIL_HOST_USER, maillist)
+
+        try:
+            start_year, start_month, start_day = start_date.split('-')
+            end_year, end_month, end_day = end_date.split('-')
+            valid = False
+            if end_year > start_year or (end_year == start_year and end_month > start_month):
+                valid = True
+            elif end_year == start_year and end_month == start_month and end_day >= start_day:
+                valid = True
+            else:
+                valid = False
+            if valid:
+
+                if msg.send():
+
+                    messages.success(
+                        request, 'Leave request submitted successfully.')
+                    for members in mem:
+                        username = members.username
+                        if username == request.user.username:
+                            # members.req_sent = True
+                            # members.save(update_fields=['req_sent'])
+                            leave_req = Leave(
+                                start_date=start_date, end_date=end_date, reason=reason, user_id=members.user_id)
+                            leave_req.save()
+
+                else:
+                    messages.error(request, 'Leave request unsuccessful.')
+            else:
+                messages.error(request, "Please mention valid time period")
+        except:
+            messages.error(
+                request, "Leave request unsuccessful, please give valid details.")
+
     return render(request, 'dashboard/leave_request.html')
 
 def user(request, id):
     user = get_object_or_404(Members, id=id)
     members = Members.objects.all()
     leave= Leave.objects.all()
+    # today= date.today()
+    if request.method == 'POST':
+        start_dates = request.POST.get('start-date')
+        end_dates = request.POST.get('end-date')
+        start_date = datetime.strptime(start_dates, '%Y-%m-%d').date()
+        print(type(start_date))
+        print(start_date)
+        end_date = datetime.strptime(end_dates, '%Y-%m-%d').date()
+        print(end_date)  
+        n_days=0
+        n_dayst= (end_date-start_date).days
+        for users in members:
+            if users.id==id:
+                print(id)
+                user_id=users.user_id
+                print(user_id)
+        for req in leave:
+            if req.user_id==user_id:
+                start=req.start_date
+                end=req.end_date
+                if start>end_date or end<start_date:
+                    print("no")
+                    continue
+
+                if start>=start_date and end<=end_date:
+                    print("y")
+                    n_days+= (end-start).days
+                if end>end_date and start>=start_date:
+                    print("e")
+                    n_days+= (end_date-start).days
+                if end<=end_date and start<start_date:
+                    print("s")
+                    n_days+= (end-start_date).days
+        print(n_days)
+        print(n_dayst)
+        a= n_days
+        b= n_dayst
+        days_present=b-a
+        dict={'n_days':n_days,'n_dayst':n_dayst, 'days_present': days_present }
+   
+        context = {'members': members, 'id': id, 'leave': leave, 'dict': dict}
+        return render(request, 'dashboard/user.html', context)
     context = {'members': members, 'id': id, 'leave': leave}
     return render(request, 'dashboard/user.html', context)
 
@@ -59,10 +152,36 @@ def approve(request):
                         dict[mentee_name] = req.user_id
     print(dict)
 
-  
- 
+    # status = request.POST.get('status')
+    # req_id = request.POST.get('request_id')
+
+    # print(req_id)
+
+    # req = requests.objects.get(id=id)
+    if request.method == 'POST':
+        print(request.POST.keys())
+        reqe = requests.get(id=request.POST.get('request_id'))
+        user_id= reqe.user_id
+        for user in members:
+            if user.user_id==user_id:
+                user_mail= user.email
+        maillist=[user_mail]
+        
+        if 'approved' in request.POST.keys():
+            reqe.status = "approved"
+            reqe.save(update_fields=['status'])            
+            msg = EmailMultiAlternatives(
+            'Leave request status', f'Your leave request from {reqe.start_date} to {reqe.end_date} has been approved ', EMAIL_HOST_USER, maillist)
+            msg.send()
+            return HttpResponseRedirect(request.path_info)
+        elif 'rejected' in request.POST.keys():
+            reqe.status = "rejected"
+            reqe.save(update_fields=['status'])
+            msg = EmailMultiAlternatives(
+            'Leave request status', f'Your leave request from {reqe.start_date} to {reqe.end_date} has been rejected ', EMAIL_HOST_USER, maillist)
+            msg.send()
+            return HttpResponseRedirect(request.path_info)
     
 
     return render(request, 'dashboard/approve.html', {'members': members, 'requests': requests, 'dict': dict})
-
 
